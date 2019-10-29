a0132f3aba2734add49e2b4d38f2ae257151f6fb
marcelabc <mbc3@cin.ufpe.br>
2019-10-29 10:57:25 -0300

    LPS-103629 Creation of a new field: Separator

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/separator/SeparatorDDMFormFieldTemplateContextContributor.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.field.type.internal.separator;
+
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTemplateContextContributor;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.model.LocalizedValue;
+import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
+import com.liferay.petra.string.StringPool;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marcela Cunha
+ */
+@Component(
+	immediate = true, property = "ddm.form.field.type.name=separator",
+	service = {
+		DDMFormFieldTemplateContextContributor.class,
+		SeparatorDDMFormFieldTemplateContextContributor.class
+	}
+)
+public class SeparatorDDMFormFieldTemplateContextContributor
+	implements DDMFormFieldTemplateContextContributor {
+
+	@Override
+	public Map<String, Object> getParameters(
+		DDMFormField ddmFormField,
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
+
+		Map<String, Object> parameters = new HashMap<>();
+
+		parameters.put(
+			"style", getStyle(ddmFormField, ddmFormFieldRenderingContext));
+
+		return parameters;
+	}
+
+	protected String getStyle(
+		DDMFormField ddmFormField,
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
+
+		LocalizedValue style = (LocalizedValue)ddmFormField.getProperty(
+			"style");
+
+		if (style == null) {
+			return StringPool.BLANK;
+		}
+
+		return style.getString(ddmFormFieldRenderingContext.getLocale());
+	}
+
+}
